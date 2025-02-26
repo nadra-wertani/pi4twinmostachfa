cbea63771a41f502f2ed8b738b3beeae0813900f
nadra <nadrawertani22@gmail.com>
2025-02-26 01:52:17 +0100

    ddx thrhrcryestncccccccccsssffz

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ const app = express();
 // Configuration et middleware
 
  // ⚠️ Déclarer `app` AVANT d'utiliser `app.use()`
- mongoose
-  .connect("mongodb://127.0.0.1:27017/regpidecodequeen")
-  .then(() => console.log("Connexion à MongoDB réussie.")) // Log de succès
-  .catch((err) => console.error("Erreur de connexion à MongoDB:", err)); // Log d'erreur
+ const mongoose = require('mongoose');
 
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.error("MongoDB connection error:", err));
+    
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 app.use(express.json());
